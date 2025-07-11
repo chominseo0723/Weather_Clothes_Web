8da89f7e94e38997a6e341afa7c40b0e0d4499d6
chominseo0723 <singer100@yu.ac.kr>
2025-07-11 12:45:51 +0900

    home화면 틀

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
@@ -15,6 +16,13 @@ const Home = () => {
         <li className='hover:font-bold'>세일</li>
        </ul>
     </div>
+       <div className='flex gap-5 justify-center mt-100 '>
+        <Link to='/minprice' className='border py-1 px-3 rounded-lg border-gray-300'>오늘의 최저가</Link>
+        <Link to='/timesale' className='border py-1 px-3 rounded-lg  border-gray-300'>타임세일</Link>
+        <Link to='/freedrive' className='border py-1 px-3 rounded-lg  border-gray-300'>무료배송</Link>
+        <Link to='/random' className='border py-1 px-3 rounded-lg  border-gray-300'>꽝 없는 뽑기</Link>
+       </div>
+    <span className='flex mt-20 ml-10 text-2xl font-bold'>날씨별 종목 추천</span>
     </>
   )
 }
